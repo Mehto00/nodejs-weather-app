09badabd8cff5015c5bc7aefe921aa6004068967
Mikko Metso <mikkosanterimetso@gmail.com>
2017-10-06 12:05:58 +0300

    Set default location to Helsinki. Added function for formating input style. Added try/catch to handle some errors

--- app.js
+++ app.js
@@ -9,30 +9,51 @@
 const https = require('https');
 const api = require('./api');
 
-let location;
+let location = "Helsinki"; // Set default location to Helsinki
 
 function convertToCelsius(kelvin) {
 	celsius = kelvin - 273.15
 	return celsius;
 }
 
-function getWeather(api, location) {
-	https.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${api.key}`, (res) => {
-	let body = "";
-	res.on('data', (data) => {
-		body += data.toString();
-
-	}).on('end', () => {
-		const weatherData = JSON.parse(body);
-		let celsius = convertToCelsius(weatherData.main.temp);
-		console.log(`The temperature in ${location} is currently ${celsius.toFixed(1)} degrees celsius`);
-	});
-
-	}).on('error', (e) => {
-		console.error(e);
-	});
+function formatInputStyle(userInput) {
+	userInput = userInput.charAt(0).toUpperCase() + userInput.substr(1).toLowerCase()
+	return userInput;
 }
 
-location = process.argv.slice(2,3);
+function getWeather(api, location) {
+	https.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${api.key}`, (res) => {
+		let body = "";
+		
+		res.on('data', (data) => {
+			body += data.toString();
+
+		}).on('end', () => {
+			try {
+				const weatherData = JSON.parse(body);
+				var weatherDescription = weatherData.weather[0].description;
+				let celsius = convertToCelsius(weatherData.main.temp);
+
+				// Print the results
+				console.log(`The Weather description for ${location} is currently ${weatherDescription}`);
+				console.log(`and the temperature is ${celsius.toFixed(1)} degrees celsius`);
+			} catch(e) {
+				console.log(`Problem with user input: No data was found for ${location}`)
+			}
+		});
+
+	}).on('error', (e) => {
+		console.error(e.message);
+	});
+} 
+
+// Update location if given and format style 
+if (process.argv.slice(2,3) != "") {
+	location = process.argv.slice(2,3);
+	location = formatInputStyle(location.toString());
+}
 
 getWeather(api, location);
+
+
+
